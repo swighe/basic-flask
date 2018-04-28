70a5c240d95f4be4a39e8d84c18a80e77b4cf6bd
James Lin <jameslin@appnexus.com>
2018-04-28 10:28:31 -0400

    also delete

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ def add_name():
 	list_of_names.append(request.form['name'])
 	return redirect('/')
 
+@app.route('/delete-name/<int:index_plus_one>', methods=['POST'])
+def del_name(index_plus_one):
+	list_of_names.pop(index_plus_one - 1)
+	return redirect('/')
+
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host="0.0.0.0", port=port, threaded=True)
