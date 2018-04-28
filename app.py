febed186e59cd6785b7fe64028ca97eda6fae3f4
James Lin <jameslin@appnexus.com>
2018-04-27 22:08:54 -0400

    basic form

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import os
 
 app = Flask(__name__)
 
+list_of_names = []
+
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return render_template('home.html', names=list_of_names)
+
+@app.route('/add-name', methods=['POST'])
+def add_name():
+	list_of_names.append(request.form['name'])
+	return redirect('/')
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
