e441298f95b45ab49857e5668b24be8b8acfd443
James Lin <jameslin@appnexus.com>
2018-07-15 16:11:24 -0400

    more testing

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, Response, request
 import os
+import pprint
 
 app = Flask(__name__)
 
 @app.route('/fulfill', methods=['POST'])
 def home():
-	print request.form.to_dict()
+	print request.form
+	print request.query_string
 	return Response(
 		'{"displayText":"this response came from fulfillment","speech":"SAY SOMETHING"}',
 		mimetype='application/json'
@@ -14,4 +16,4 @@ def home():
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
-	app.run(host="0.0.0.0", port=port, threaded=True)
+	app.run(host="0.0.0.0", port=port, threaded=True, debug=True)
