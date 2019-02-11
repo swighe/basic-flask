02fcdff7277c773d8e13128923eb17b7ebce2442
James Lin <jameslin@appnexus.com>
2019-02-11 10:58:35 -0500

    a few more debugging things

--- app.py
+++ app.py
@@ -16,10 +16,19 @@ select_users_where = 'SELECT * FROM user WHERE username = %s'
 select_users = 'SELECT * FROM user'
 
 def create_mysql_connection():
-	connection = mysql.connector.connect(user=os.environ.get('AWS_USER'), password=os.environ.get('AWS_PASS'),
+	try:
+		cnx = mysql.connector.connect(user=os.environ.get('AWS_USER'), password=os.environ.get('AWS_PASS'),
 								host=os.environ.get('AWS_HOST'), database=os.environ.get('AWS_DB'))
-	print(connection)
-	return connection
+		return cnx
+	except mysql.connector.Error as err:
+		if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+			print("Something is wrong with your user name or password")
+		elif err.errno == errorcode.ER_BAD_DB_ERROR:
+			print("Database does not exist")
+		else:
+			print(err)
+	else:
+		cnx.close()
 
 def write_data(sql_command, data):
 	cnx = create_mysql_connection()
